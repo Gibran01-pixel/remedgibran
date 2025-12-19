7da17a16a4ed9148c2fcf86d04e7cb27633b12f9
Gibran01-pixel <msaidgibran@gmail.com>
2025-12-19 14:42:22 +0700

    migration & model

---
+++ app/Models/DetailPemesanan.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DetailPemesanan extends Model
+{
+    protected $fillable = [
+        'pemesanan_id',
+        'menu_id',
+        'jumlah',
+        'subtotal',
+    ];
+
+    public function pemesanan()
+    {
+        return $this->belongsTo(Pemesanan::class);
+    }
+
+    public function menu()
+    {
+        return $this->belongsTo(Menu::class);
+    }
+}
+
---
+++ app/Models/Pemesanan.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pemesanan extends Model
+{
+    protected $fillable = [
+        'pelanggan_id',
+        'tanggal_pesan',
+        'tanggal_acara',
+        'total_harga',
+        'status',
+    ];
+
+    public function pelanggan()
+    {
+        return $this->belongsTo(Pelanggan::class);
+    }
+
+    public function detailPemesanans()
+    {
+        return $this->hasMany(DetailPemesanan::class);
+    }
+}
+
